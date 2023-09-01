329f94bb2238c267a0c2029eddec55e9cef522e5
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-01 05:27:13 -0700

    Initial Commit

---
+++ spendwise/spendwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  spendwiseApp.swift
+//  spendwise
+//
+//  Created by kaveenprabodhya on 2023-09-01.
+//
+
+import SwiftUI
+
+@main
+struct spendwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
