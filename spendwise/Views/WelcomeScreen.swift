bd7858357d2681e148a1a36126b98686501aa673
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-01 13:29:16 -0700

    add welcome screen

---
+++ spendwise/Views/WelcomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeScreen.swift
+//  spendwise
+//
+//  Created by kaveenprabodhya on 2023-09-01.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        Text("Welcome Screen.")
+    }
+}
+
+struct WelcomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreen()
+    }
+}
