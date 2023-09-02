3007e4ea1ceb38f59a3aef0cf69775aa73863210
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-02 14:36:38 -0700

    add custom shape view CurvedSidedRectangle

---
+++ spendwise/Utilities/CurvedSideRectangle.swift
@@ -0,0 +1,27 @@
+//
+//  CurvedSideRectangle.swift
+//  spendwise
+//
+//  Created by kaveenprabodhya on 2023-09-02.
+//
+
+import SwiftUI
+
+struct CurvedSideRectangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint.zero)
+        path.addLine(to: CGPoint(x: rect.maxX, y: 0))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addQuadCurve(to: CGPoint(x:0, y: rect.maxY), control: CGPoint(x: rect.midX, y: rect.maxY + 60))
+        path.closeSubpath()
+        return path
+    }
+}
+
+struct CurvedSideRectangle_Previews: PreviewProvider {
+    static var previews: some View {
+        CurvedSideRectangle()
+            .frame(height: 300)
+    }
+}
