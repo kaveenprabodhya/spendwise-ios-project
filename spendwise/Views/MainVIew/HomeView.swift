16911bce2836389e7d4f9a52a0899628b3aa8a53
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-03 13:44:11 -0700

    add Barnhart to the home and finish top canvas

--- spendwise/Views/MainVIew/HomeView.swift
+++ spendwise/Views/MainVIew/HomeView.swift
@@ -10,23 +10,115 @@ import SwiftUI
 struct HomeView: View {
     
     var body: some View {
-        VStack {
-            VStack{
-                CurvedSideRectangle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [Color("ColorLavenderPurple"), Color("ColorTurquoiseBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width: 430, height: 491)
-                    .edgesIgnoringSafeArea(.top)
-                    .overlay {
-                        Text("Hello")
-                            .foregroundColor(.white)
-                    }
-                Spacer()
-            }
-            VStack{
-                Text("Something")
-                Spacer()
+        ScrollView{
+            VStack(spacing: 20) {
+                VStack{
+                    CurvedSideRectangleViewShape()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color("ColorLavenderPurple"), Color("ColorTurquoiseBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 430, height: 491)
+                        .edgesIgnoringSafeArea(.top)
+                        .overlay {
+                            VStack{
+                                VStack{
+                                    CustomTopNavigationView()
+                                }
+                                .padding(.top, 50)
+                                VStack(spacing: 15){
+                                    Spacer()
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .stroke(Color("ColorLightGray"), lineWidth: 3)
+                                        .frame(width: 130, height: 41)
+                                        .overlay {
+                                            Text("Octomber")
+                                                .font(.system(size: 22))
+                                                .foregroundColor(.white)
+                                                .fontWeight(.medium)
+                                        }
+                                        .padding(5)
+                                    Ellipse()
+                                        .fill(Color("ColorSnowWhite"))
+                                        .frame(width: 52, height:52)
+                                    VStack(spacing: 8) {
+                                        Text("Your available balance is")
+                                            .foregroundColor(.white)
+                                        Text("20,983")
+                                            .font(.system(size: 28))
+                                            .foregroundColor(.white)
+                                        Text("By this time last month, you spent slightly higher (22,719)")
+                                            .frame(width: 250)
+                                            .lineLimit(nil)
+                                            .foregroundColor(.white)
+                                            .multilineTextAlignment(.center)
+                                            .fixedSize(horizontal: false, vertical: true)
+                                    }
+                                }
+                                VStack {
+                                    Spacer()
+                                    Spacer()
+                                    HStack(spacing: 20) {
+                                        RoundedRectangle(cornerRadius: 20.0)
+                                            .fill(Color("ColorEmeraldGreen"))
+                                            .frame(width: 164, height: 82, alignment: .bottomLeading)
+                                            .overlay {
+                                                HStack {
+                                                    RoundedRectangle(cornerRadius: 20.0)
+                                                        .fill(Color("ColorSnowWhite"))
+                                                        .frame(width: 48, height: 49, alignment: .leading)
+                                                        .overlay{
+                                                            Image(systemName: "square.and.arrow.down.fill")
+                                                                .font(.system(size: 26))
+                                                                .foregroundColor(Color("ColorEmeraldGreen"))
+                                                        }
+                                                    VStack(alignment: .leading, spacing: 10) {
+                                                        Text("Income")
+                                                            .foregroundColor(Color("ColorSnowWhite"))
+                                                            .font(.system(size: 14))
+                                                        Text("LKR 5000")
+                                                            .foregroundColor(Color("ColorSnowWhite"))
+                                                            .font(.system(size: 16))
+                                                            .fontWeight(.semibold)
+                                                    }
+                                                }
+                                                .padding(5)
+                                            }
+                                        RoundedRectangle(cornerRadius: 20.0)
+                                            .fill(Color("ColorRustyRed"))
+                                            .frame(width: 164, height: 82, alignment: .bottomLeading)
+                                            .overlay {
+                                                HStack {
+                                                    RoundedRectangle(cornerRadius: 20.0)
+                                                        .fill(Color("ColorSnowWhite"))
+                                                        .frame(width: 48, height: 49, alignment: .leading)
+                                                        .overlay{
+                                                            Image(systemName: "square.and.arrow.up.fill")
+                                                                .font(.system(size: 26))
+                                                                .foregroundColor(Color("ColorRustyRed"))
+                                                        }
+                                                    VStack(alignment: .leading, spacing: 10) {
+                                                        Text("Expense")
+                                                            .foregroundColor(Color("ColorSnowWhite"))
+                                                            .font(.system(size: 14))
+                                                        Text("LKR 5000")
+                                                            .foregroundColor(Color("ColorSnowWhite"))
+                                                            .font(.system(size: 16))
+                                                            .fontWeight(.semibold)
+                                                    }
+                                                }
+                                                .padding(5)
+                                            }
+                                    }
+                                    Spacer()
+                                }
+                            }
+                        }
+                }
+                .padding(.bottom, 40)
+                VStack{
+                    WeeklyBarChartView()
+                }
             }
         }
+        .ignoresSafeArea(.all)
     }
 }
 
