bb74f6b590f440afc2dcc3bfddc16edbf16ca981
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-05 02:36:56 -0700

    create base view of bottom navigation

--- spendwise/Utilities/BottomNavigationBarView.swift
+++ spendwise/Utilities/BottomNavigationBarView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct BottomNavigationBarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.blue.background(.ultraThinMaterial)
+            Text("Overlay at the Bottom")
+                .foregroundColor(.white)
+        }
+        .frame(height: 81)
+        .frame(maxHeight: .infinity, alignment: .bottom)
     }
 }
 
