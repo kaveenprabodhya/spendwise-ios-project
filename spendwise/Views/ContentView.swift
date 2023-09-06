009d85d5a7f96ab6b797e8d965d0da6865510637
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-05 17:59:07 -0700

    conditionally loading home and onboarding views

--- spendwise/Views/ContentView.swift
+++ spendwise/Views/ContentView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
     var body: some View {
-       HomeView()
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                ZStack {
+//                    TabView{
+//                        Group {
+//                            HomeView()
+//                                .tabItem {
+//                                    Label("Home", systemImage: "house.fill")
+//                                }
+//                            TransactionView()
+//                                .tabItem {
+//                                    Label("Transactions", systemImage: "arrow.left.arrow.right.square")
+//                                }
+//                            BudgetView()
+//                                .tabItem {
+//                                    Label("Budget", systemImage: "chart.pie.fill")
+//                                }
+//                            AccountView()
+//                                .tabItem {
+//                                    Label("Budget", systemImage: "person.crop.circle")
+//                                }
+//                        }
+//                        .toolbarBackground(Color("ColorElectricIndigo"), for: .tabBar)
+//                        .toolbarBackground(.visible, for: .tabBar)
+//                    }
+                    HomeView()
+                    
+                }
+            }
+        }
     }
 }
 
