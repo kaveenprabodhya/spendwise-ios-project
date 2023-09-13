1a64521b223d41094a006a3543861af2de81c985
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-13 15:29:19 -0700

    add BudgetViewModel and HomeViewModel

---
+++ spendwise/Views/MainVIew/Budget/ViewModels/BudgetViewModel.swift
@@ -0,0 +1,74 @@
+//
+//  BudgetViewModel.swift
+//  spendwise
+//
+//  Created by kaveenprabodhya on 2023-09-13.
+//
+
+import SwiftUI
+
+class BudgetViewModel: ObservableObject{
+    @Published var budgetArray:[Budget] = [
+        Budget(
+            id: UUID(),
+            budgetType: BudgetType(type: .monthly, date: .monthOnly(9), limit: 2500),
+            category:
+                [
+                    BudgetCategory(
+                        id: UUID(),
+                        name: "Shopping",
+                        primaryBackgroundColor: "ColorGoldenrod"
+                    )
+                ],
+            allocatedAmount: 300000.00,
+            currentAmountSpent: 100000.00,
+            numberOfDaysSpent: 8,
+            footerMessage: FooterMessage(message: "You’ve exceed the limit!", warning: true)
+        ),
+        Budget(
+            id: UUID(),
+            budgetType: BudgetType(type: .yearly, date: .yearOnly(2023), limit: 8000),
+            category:
+                [
+                    BudgetCategory(
+                        id: UUID(),
+                        name: "Transportation",
+                        primaryBackgroundColor: "ColorVividBlue"
+                    )
+                ],
+            allocatedAmount: 300000.00,
+            currentAmountSpent: 100000.00,
+            numberOfDaysSpent: 8,
+            footerMessage: FooterMessage(message: "You’ve exceed the limit!", warning: false)
+        ),
+        Budget(
+            id: UUID(),
+            budgetType: BudgetType(type: .weekly, date: .dateRange(10, 15, 22), limit: 1200),
+            category:
+                [
+                    BudgetCategory(
+                        id: UUID(),
+                        name: "Groceries",
+                        primaryBackgroundColor: "ColorSecondTealGreen"
+                    )
+                ],
+            allocatedAmount: 300000.00,
+            currentAmountSpent: 100000.00,
+            numberOfDaysSpent: 8,
+            footerMessage: FooterMessage(message: "You are doing really great! ", warning: true)
+        )
+    ]
+    
+    func fetchData() {
+        ApiService.fetchBudgetData { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let budgetItems):
+                    self.budgetArray = budgetItems
+                case .failure(let error):
+                    print("Error fetching data: \(error)")
+                }
+            }
+        }
+    }
+}
---
+++ spendwise/Views/MainVIew/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  spendwise
+//
+//  Created by kaveenprabodhya on 2023-09-13.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+}
