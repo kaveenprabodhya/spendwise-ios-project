29341884f6adae6e964c61357742ffdd0bbd2da8
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-13 15:25:22 -0700

    add ApiService

---
+++ spendwise/Services/ApiService.swift
@@ -0,0 +1,24 @@
+//
+//  ApiService.swift
+//  spendwise
+//
+//  Created by kaveenprabodhya on 2023-09-13.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case requestFailed
+    case invalidResponse
+    case decodingError
+}
+
+class ApiService {
+    static func fetchBudgetData(completion: @escaping (Result<[Budget], NetworkError>) -> Void) {
+        guard let url = URL(string: "https://your-api-url") else {
+            completion(.failure(.invalidURL))
+            return
+        }
+    }
+}
