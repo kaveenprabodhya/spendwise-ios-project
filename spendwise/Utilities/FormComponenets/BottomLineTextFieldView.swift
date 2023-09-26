547318928c5269b193d62e4d606f804cff6fbcd9
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-26 15:35:55 -0700

    add fontcolor and label color as optional

--- spendwise/Utilities/FormComponenets/BottomLineTextFieldView.swift
+++ spendwise/Utilities/FormComponenets/BottomLineTextFieldView.swift
@@ -10,31 +10,38 @@ import SwiftUI
 struct BottomLineTextFieldView: View {
     var label: String
     var placeholder: String
+    var fontColor: Color?
+    var bottomLineColor: Color?
+    var labelSize: CGFloat?
+    var textFieldFontSize: CGFloat?
     @Binding var textInputVal: String
     
     var body: some View {
         VStack {
             if !label.isEmpty {
                 Text("\(label)")
-                    .font(.system(size: 18, weight:  .medium))
+                    .foregroundColor(fontColor != nil ? fontColor! : .black)
+                    .font(.system(size: labelSize != nil ? labelSize! : 18, weight:  .medium))
                     .frame(maxWidth: .infinity, alignment: .leading)
             }
             TextField("\(placeholder)", text: $textInputVal)
-                .foregroundColor(.black)
+                .foregroundColor(fontColor != nil ? fontColor! : .black)
                 .textFieldStyle(BottomLineTextFieldStyle())
-                .font(.system(size: 24))
+                .font(.system(size: textFieldFontSize != nil ? textFieldFontSize! : 24))
         }.padding()
     }
 }
 
 struct BottomLineTextFieldStyle: TextFieldStyle {
+    var bottomLineColor: Color?
+    
     func _body(configuration: TextField<_Label>) -> some View {
         VStack {
             configuration
                 .overlay(
                     Rectangle()
                         .frame(height: 2, alignment: .bottom)
-                        .foregroundColor(.black)
+                        .foregroundColor(bottomLineColor != nil ? bottomLineColor! : .black)
                         .padding(.top, 45)
                 )
         }
@@ -43,6 +50,6 @@ struct BottomLineTextFieldStyle: TextFieldStyle {
 
 struct BottomLineTextFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        BottomLineTextFieldView(label: "Name of Budget", placeholder: "Type....", textInputVal: .constant(""))
+        BottomLineTextFieldView(label: "Name of Budget", placeholder: "Type....", fontColor: .blue, bottomLineColor: .blue, labelSize: 32, textFieldFontSize: 32, textInputVal: .constant(""))
     }
 }
