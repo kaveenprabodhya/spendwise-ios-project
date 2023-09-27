0b6b288207c824779a3669cfddcc2c37b57563a2
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-26 21:31:27 -0700

    make select option dynamic

---
+++ spendwise/Views/MainVIew/Transaction/NewTransactionView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTransactionView.swift
+//  spendwise
+//
+//  Created by kaveenprabodhya on 2023-09-26.
+//
+
+import SwiftUI
+
+struct NewTransactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTransactionView()
+}
