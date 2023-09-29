b3fbf77000f74aa128336b5d9d674d93862c4c4d
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-29 00:38:14 -0700

    confing signin view

--- spendwise/Views/OnboardingView/Authentication/SigninView.swift
+++ spendwise/Views/OnboardingView/Authentication/SigninView.swift
@@ -8,8 +8,146 @@
 import SwiftUI
 
 struct SigninView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State var emailInputVal: String = ""
+    @State var passwordInputVal: String = ""
+    @State var isSigninSuccess: Bool = false
+    @State var isSignupClick: Bool = false
+    
     var body: some View {
-        Text("Signin view")
+        NavigationStack{
+            VStack {
+                Circle()
+                    .fill(.clear)
+                    .frame(width: 320, height: 280)
+                    .overlay {
+                        Image("login-screen")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                    .padding(.vertical, 8)
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Welcome Back")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 24, weight: .semibold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("Hey you’re back, fill in your details to log in")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 18, weight: .medium))
+                }
+                .padding(.horizontal, 15)
+                .padding(.bottom, 18)
+                VStack {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.white)
+                        .frame(width: 391, height: 62)
+                        .overlay {
+                            HStack {
+                                TextField(text: $emailInputVal) {
+                                    Text("Email")
+                                        .foregroundStyle(Color("ColorSteelGray"))
+                                        .font(.system(size: 20, weight: .medium))
+                                }
+                                .foregroundStyle(Color("ColorSteelGray"))
+                                
+                            }
+                            .padding(.horizontal, 10)
+                        }
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.white)
+                        .frame(width: 391, height: 62)
+                        .overlay {
+                            HStack {
+                                SecureField(text: $passwordInputVal) {
+                                    Text("Password")
+                                        .foregroundStyle(Color("ColorSteelGray"))
+                                        .font(.system(size: 20, weight: .medium))
+                                }
+                                .foregroundStyle(Color("ColorSteelGray"))
+                                Image(systemName: "eye")
+                                    .foregroundStyle(Color("ColorSteelGray"))
+                                    .font(.system(size: 20, weight: .medium))
+                            }
+                            .padding(.horizontal, 10)
+                        }
+                    VStack {
+                        Text("Forget password?")
+                            .foregroundStyle(.white)
+                        .font(.system(size: 20))
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .padding(.top, 15)
+                    .padding(.trailing, 15)
+                    .padding(.bottom, 18)
+                    Spacer()
+                    HStack {
+                        Button {
+                            isSignupClick = true
+                        } label: {
+                            RoundedRectangle(cornerRadius: 25)
+                                .fill(Color("ColorGoldenrod"))
+                                .frame(width: 113, height: 48)
+                                .overlay {
+                                    Text("Sign up")
+                                        .font(.system(size: 16, weight: .medium))
+                                        .foregroundStyle(.white)
+                                }
+                        }
+                        .buttonStyle(GrowButton())
+                        .navigationDestination(isPresented: $isSignupClick) {
+                            SignupView()
+                        }
+                        Spacer()
+                        Button {
+                            isOnboardingViewActive = false
+                            isSigninSuccess = true
+                        } label: {
+                            RoundedRectangle(cornerRadius: 25)
+                                .fill(Color("ColorElectricIndigo"))
+                                .frame(width: 186, height: 67)
+                                .overlay {
+                                    Text("Sign in")
+                                        .font(.system(size: 16, weight: .medium))
+                                        .foregroundStyle(.white)
+                                }
+                        }
+                        .buttonStyle(GrowButton())
+                        .navigationDestination(isPresented: $isSigninSuccess) {
+                            ContentView()
+                        }
+                    }
+                    .padding(.horizontal, 15)
+                }
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(LinearGradient(
+                gradient: Gradient(colors: [Color("ColorElectricIndigo"), Color("ColorCerulean")]),
+                startPoint: .top,
+                endPoint: .bottom
+            ))
+            .navigationBarBackButtonHidden(true)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    VStack {
+                        Text("Sign in")
+                            .font(.system(size: 22, weight: .semibold))
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct GrowButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
 }
 
