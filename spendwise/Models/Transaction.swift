330eaf3d073ee4cd6fa8f8fec1c73bf8aa98aa53
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-09-29 22:43:25 -0700

    config ui of transaction

--- spendwise/Models/Transaction.swift
+++ spendwise/Models/Transaction.swift
@@ -1,11 +1,25 @@
 //
-//  Expense.swift
+//  Income.swift
 //  spendwise
 //
 //  Created by kaveenprabodhya on 2023-09-08.
 //
 
 import Foundation
+
+struct Income: Identifiable {
+    let id: String
+    let date: Date
+    let amount: Double
+    let source: String
+    let description: String
+    let recurring: RecurringIncome
+}
+
+struct RecurringIncome {
+    let frequency: String
+    let date: String
+}
 
 struct Expense: Identifiable {
     let id: String
---
+++ spendwise/Views/MainVIew/Transaction/ViewModels/TransactionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionViewModel.swift
+//  spendwise
+//
+//  Created by kaveenprabodhya on 2023-09-29.
+//
+
+import Foundation
